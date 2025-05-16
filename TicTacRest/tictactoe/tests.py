46ce91e127759de44b277987c11eb3e6f61be619
Lucas Ignjatov <lucas.ignjatov@gmail.com>
2025-05-16 22:06:12 +0200

    Added names to urls
    Added tests for Joining games, craeting a user
    Added test for move making:
     - tesing that opponent takes first turn (creator is always first)
     - testing that creator takes first turn (should suceed)
     - creator taking a turn twice (turns alternate so it should fail)

--- TicTacRest/tictactoe/tests.py
+++ TicTacRest/tictactoe/tests.py
@@ -1,4 +1,68 @@
-from django.test import TestCase
-
-
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.test import APIRequestFactory, APIClient
+from tictactoe.models import User
+from rest_framework.test import force_authenticate
+from tictactoe.models import TicTacToeGame
+import time
 # Create your tests here.
+class TicTacToe(APITestCase):
+    def test_create_user(self):
+        url = reverse("user-registration")
+        response = self.client.post(url,{'username':'test1','password':'test1'})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.get().username, "test1")
+    
+
+class GameJoinTests(APITestCase):
+    def setUp(self):
+        self.creator = User.objects.create_user(username="jura", password="jura")
+        self.opponent = User.objects.create_user(username="manus", password="manus")
+        self.client = APIClient()
+        self.game = TicTacToeGame.objects.create(creator=self.creator)
+
+    def test_join_without_login(self):
+        url = reverse("game-join", kwargs={'game_id': self.game.id})
+        response = self.client.patch(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_join_with_jwt(self):
+        url = reverse("game-join", kwargs={'game_id': self.game.id})
+        response = self.client.post(reverse('user-login'), {'username': 'manus', 'password': 'manus'})
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + response.data['access'])
+        response = self.client.patch(url)
+        self.assertEqual(response.status_code, 200)
+
+
+class GameMoveTests(APITestCase):
+    def setUp(self):
+        self.creator = User.objects.create_user(username="jura", password="jura")
+        self.opponent = User.objects.create_user(username="manus", password="manus")
+        self.client = APIClient()
+        self.game = TicTacToeGame.objects.create(creator=self.creator, opponent=self.opponent, game_state=TicTacToeGame.GameState.IN_PROGRESS)
+        
+    def test_first_move_opponent(self):
+        url = reverse("game-move", kwargs={'game_id': self.game.id})
+        response = self.client.post(reverse('user-login'), {'username': 'manus', 'password': 'manus'})
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + response.data['access'])
+        response = self.client.post(url, data={"row":"1", "col":"1"})
+        self.assertEqual(response.status_code, 403)
+    
+    def test_creator_move_first(self):
+        url = reverse("game-move", kwargs={'game_id': self.game.id})
+        response = self.client.post(reverse('user-login'), {'username': 'jura', 'password': 'jura'})
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + response.data['access'])
+        response = self.client.post(url, data={"row":"1", "col":"1"})
+        self.assertEqual(response.status_code, 200)
+    
+    def test_move_twice(self):
+        url = reverse("game-move", kwargs={'game_id': self.game.id})
+        response = self.client.post(reverse('user-login'), {'username': 'jura', 'password': 'jura'})
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + response.data['access'])
+        response = self.client.post(url, data={"row":"1", "col":"1"})
+        response = self.client.post(url, data={"row":"0", "col":"1"})
+        self.assertEqual(response.status_code, 403)
+    
+    
+        
--- TicTacRest/tictactoe/urls.py
+++ TicTacRest/tictactoe/urls.py
@@ -8,15 +8,15 @@ urlpatterns = [
     #authentication operation
     path("user/", views.UserList.as_view()),
     path("user/<str:username>/profile/", views.UserProfile.as_view()),
-    path("user/register/", views.UserCreate.as_view()),
-    path("user/login/", TokenObtainPairView.as_view()),
-    path("user/refresh/", TokenRefreshView.as_view()),
+    path("user/register/", views.UserCreate.as_view(), name="user-registration"),
+    path("user/login/", TokenObtainPairView.as_view(), name="user-login"),
+    path("user/refresh/", TokenRefreshView.as_view(), name="user-refresh"),
     #game operations
-    path("game/",views.GamesList.as_view()),
-    path("game/open/", views.GameCreate.as_view()),
-    path("game/<int:game_id>/join/", views.JoinGame.as_view()),
-    path("game/<int:game_id>/board/", views.get_board_state),
-    path("game/<int:game_id>/move/", views.make_move),
+    path("game/",views.GamesList.as_view(), name="game-all"),
+    path("game/open/", views.GameCreate.as_view(), name="game-open"),
+    path("game/<int:game_id>/join/", views.JoinGame.as_view(), name="game-join"),
+    path("game/<int:game_id>/board/", views.get_board_state, name="game-board"),
+    path("game/<int:game_id>/move/", views.make_move, name="game-move"),
     #drf-spectacular urls:
     path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
